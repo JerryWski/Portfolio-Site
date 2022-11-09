fcccf102eccb1ff3db1f7822c03e8d79059ab037
JerryWski <jarek_wasilewski@yahoo.com>
2022-11-09 23:47:47 +0100

    styling button

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const hamburgerBtn = document.querySelector('.nav__hamburger');
+const navList = document.querySelector('.nav__list');
+
+const toggleButton = () => {
+    navList.classList.toggle('show')
+}
+
+
+hamburgerBtn.addEventListener('click', toggleButton)
