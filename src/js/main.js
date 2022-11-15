d5f2abd286d5b93e56cad992c176bd91dc9d50a1
JerryWski <jarek_wasilewski@yahoo.com>
2022-11-15 15:34:48 +0100

    explanation in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,18 @@
 const hamburgerBtn = document.querySelector('.hamburger');
 const navList = document.querySelector('.nav__list');
-const links = document.querySelectorAll('.nav__links')
-console.log(links);
+
 
 const toggleButton = () => {
     hamburgerBtn.classList.toggle('is-active');
     navList.classList.toggle('active');
     document.body.classList.toggle('sticky-body');
 }
-
+//this func removes above parameters so that's why page is again scrollable
 const removeSticky = () => {
     document.body.classList.remove('sticky-body');
     navList.classList.remove('active');
     hamburgerBtn.classList.remove('is-active');
 }
-
-// const removeNav = () => {
-//     links.forEach(li => {
-//       li.classList.remove('active')  
-//     });
-// }
-
 
 hamburgerBtn.addEventListener('click', toggleButton);
 navList.addEventListener('click', removeSticky);
