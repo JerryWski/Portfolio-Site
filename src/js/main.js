43ebb60993ac5a51a039589ef542701b2ef10f0c
JerryWski <jarek_wasilewski@yahoo.com>
2022-11-15 15:26:57 +0100

    fixed toggle mobile nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,26 @@
 const hamburgerBtn = document.querySelector('.hamburger');
-console.log(hamburgerBtn);
 const navList = document.querySelector('.nav__list');
+const links = document.querySelectorAll('.nav__links')
+console.log(links);
 
 const toggleButton = () => {
     hamburgerBtn.classList.toggle('is-active');
-    navList.classList.toggle('active')
+    navList.classList.toggle('active');
+    document.body.classList.toggle('sticky-body');
 }
 
+const removeSticky = () => {
+    document.body.classList.remove('sticky-body');
+    navList.classList.remove('active');
+    hamburgerBtn.classList.remove('is-active');
+}
 
-hamburgerBtn.addEventListener('click', toggleButton)
+// const removeNav = () => {
+//     links.forEach(li => {
+//       li.classList.remove('active')  
+//     });
+// }
+
+
+hamburgerBtn.addEventListener('click', toggleButton);
+navList.addEventListener('click', removeSticky);
