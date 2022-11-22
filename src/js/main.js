701ad3ea877e48234b41e1bb1c42029093c13c6b
JerryWski <jarek_wasilewski@yahoo.com>
2022-11-22 22:52:13 +0100

    js part and some styling for card

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,30 @@
-const hamburgerBtn = document.querySelector('.hamburger');
-const navList = document.querySelector('.nav__list');
-
+const hamburgerBtn = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav__list");
+const arrowBtns = document.querySelectorAll(".card__button");
 
 const toggleButton = () => {
-    hamburgerBtn.classList.toggle('is-active');
-    navList.classList.toggle('active');
-    document.body.classList.toggle('sticky-body');
-}
+  hamburgerBtn.classList.toggle("is-active");
+  navList.classList.toggle("active");
+  document.body.classList.toggle("sticky-body");
+};
 //this func removes above parameters so that's why page is again scrollable
 const removeSticky = () => {
-    document.body.classList.remove('sticky-body');
-    navList.classList.remove('active');
-    hamburgerBtn.classList.remove('is-active');
+  document.body.classList.remove("sticky-body");
+  navList.classList.remove("active");
+  hamburgerBtn.classList.remove("is-active");
+};
+
+for (const btn of arrowBtns) {
+  btn.addEventListener("mouseover", function () {
+    this.classList.add("hide");
+  });
 }
 
-hamburgerBtn.addEventListener('click', toggleButton);
-navList.addEventListener('click', removeSticky);
+for (const btn of arrowBtns) {
+    btn.addEventListener("mouseleave", function () {
+      this.classList.remove("hide");
+    });
+  }
+
+hamburgerBtn.addEventListener("click", toggleButton);
+navList.addEventListener("click", removeSticky);
